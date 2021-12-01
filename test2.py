8f2d3524c8da3be688e29a8b9f31b30990b015b0
Bari Smith <3abdbgo@gmail.com>
2021-12-01 12:29:54 +0200

    1 to 6 OK

--- test2.py
+++ test2.py
@@ -6,10 +6,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 
 
-# PATH = 'D:\chromedriver.exe'
+
 PATH = '/home/barismith/PycharmProjects/autotest_selenium/chromedriver'
 driver = webdriver.Chrome(PATH)
 # 1. Перейти по ссылке на главную
@@ -38,11 +40,9 @@ time.sleep(2)
 def status_current_tab():
     driver.switch_to.window(driver.window_handles[1])
     tab_title = driver.title
-    # НЕ СОВСЕМ ПОНЯЛ ВТОРУЮ ЧАСТЬ 3ГО ПУНКТА:
-     #   В ДЕЙСТВИТЕЛЬНОСТИ ПРОВЕРЕТЬ НАЗВАНИЕ 'Работа в Нетпик' ИЛИ ЖЕ ВСЕТАКИ 'Работа в Netpeak'. ПО ЭТОМУ ДЕЛАЮ 2 ВАРИАНТА (ТАК КАК ВРЕМЯ ПОДЖИМАЕТ А В СТОЛЬ ПОЗНИЙ ЧАС ПЕРЕСПРОСИТЬ НЕ МОГУ) КАКОЙ НЕ НУЖНО ТОТ ЗАКОМЕНТИРУЙТЕ
+ 
 
     target_string = 'Работа в Netpeak'
-    # target_string = 'Работа в Нетпик'
     if target_string in tab_title:
         print('Correct tab')
     else:
@@ -51,37 +51,49 @@ def status_current_tab():
 status_current_tab()
 # 4. Убедится что на странице есть кнопка "Я хочу
 #  работать в Netpeak" и на нее можно кликнуть
-# work_in_teamButton = driver.find_element_by_name('Я хочу  работать в Netpeak')
+
 def work_in_teamButton_status():
     work_in_teamButton = driver.find_element_by_xpath('/html/body/div[5]/div/div/div[2]/div/a')
     print(work_in_teamButton.is_enabled())
 work_in_teamButton_status()
 
-# 5. Вернутся на предыдущую вкладку и нажать кнопку
-#     "Личный кабинет"
+# 5. Вернутся на предыдущую вкладку и нажать кнопку  "Личный кабинет"
 driver.switch_to.window(driver.window_handles[0])
 TeamButton = driver.find_element_by_link_text('Личный кабинет')
 TeamButton.click()
-
-#
+'''Начиная с этого места и до конца у меня не получаэться найти ниже указаные обьекты через программу. 
+Все что смог найти перепробовал, но без результатно. Оставляю тот вариант который на мое субьективное мнение 
+самый подходящий'''
 # 6. На странице личного кабинета заполнить
 #     Логин и Пароль случайными данными.
 def get_random_string(length):
 
     result_str = ''.join(random.choice(string.ascii_letters) for i in range(length))
-LoginForm =driver.find_element_by_xpath('//*[@id="loginForm"]').click()
+
 username = driver.find_element(By.NAME, 'Логин').click()
-username.send_keys('get_random_string(8)')
-# password = driver.find_element_by_xpath('//*[@id="password"]').click()
-# password.send_keys(get_random_string(8))
+username.send_keys(get_random_string(8))
+password = driver.find_element_by_(By.NAME, 'Пароль"]').click()
+password.send_keys(get_random_string(8))
 
 
 # 7. Проверить что кнопка "Войти" не доступна
+submitButton= driver.find_element_by_xpath('//*[@id="loginForm"]/div[5]/button/span')
+prop = submitButton.get_property('disabled')
+print (prop)
 
 # 8. Отметить чекбокс "Авторизируясь, вы соглашаетесь с
 #     Политикой конфиденциальности"
-#
+checkbox = driver.find_element_by_xpath('//*[@id="loginForm"]/div[4]/div/md-checkbox').click()
 # 9. Нажать на кнопку войти и проверить наличие нотификации
 #     о неправильном логине или пароле
-#
+submitButton= driver.find_element_by_xpath('//*[@id="loginForm"]/div[5]/button/span').click()
+wait = WebDriverWait(driver, 10)
+wait.until(EC.alert_is_present())
+
+alert = driver.switch_to.alert
+assert "Непраильный логин или пароль" in alert.text
+alert.accept()
+
 # 10. Проверить что Логин и Пароль подсветились красным цветом
+
+#Тут вообще пока что не знаю как найти цвет и как это обработать.
